7f791d9f84aa3adabf3a37e0cfa6073819b720df
RezaKhodarahmi <mrkhodarahmii@gmail.com>
2024-01-28 13:09:54 +0330

    prod

--- src/configs/auth.js
+++ src/configs/auth.js
@@ -3,8 +3,8 @@ export default {
 
   // loginEndpoint: 'http://localhost:3200/api/v1/auth/login',
 
-  meEndpoint: 'https://idtech.ca:3200/api/v1/auth/refresh',
-  loginEndpoint: 'https://idtech.ca:3200/api/v1/auth/login',
+  meEndpoint: 'http://idtech.ca:3200/api/v1/auth/refresh',
+  loginEndpoint: 'http://idtech.ca:3200/api/v1/auth/login',
 
   registerEndpoint: '/jwt/register',
   storageTokenKeyName: 'accessToken',
